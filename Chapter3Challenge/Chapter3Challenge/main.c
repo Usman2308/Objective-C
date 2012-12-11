3fae140d068c25bfcfff75244ee54177e41e1446
Usman <usmanxna@gmail.com>
2012-12-11 21:56:06 +0000

    Adding chaper 3 challenge

---
+++ Chapter3Challenge/Chapter3Challenge/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Chapter3Challenge
+//
+//  Created by Usman Khan on 11/12/2012.
+//  Copyright (c) 2012 Usman Khan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    float a = 3.14;
+    float b = 2.67;
+    
+    double total = a + b;
+    
+    printf("%f", total);
+    
+    return 0;
+}
+
