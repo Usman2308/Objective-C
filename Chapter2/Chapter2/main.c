1a26bf76d054e6c6a4cfae1c79039c9673c8fed3
Usman <usmanxna@gmail.com>
2012-12-11 21:33:33 +0000

    Adding Chapter 2

---
+++ Chapter2/Chapter2/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Chapter2
+//
+//  Created by Usman Khan on 11/12/2012.
+//  Copyright (c) 2012 Usman Khan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]){
+    
+    //print the beginning of the novel
+    printf("It was the best of times. \n");
+    printf("It was the worst of times. \n");
+    
+    return 0;
+}
+
